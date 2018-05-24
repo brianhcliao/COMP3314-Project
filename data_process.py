3c9f0774f32113815366ccf2395d59e8f5d13f22
Jimmy Lee <39530078+jimmyshuyulee@users.noreply.github.com>
2018-05-24 21:59:14 +0800

    Update data_process.py

--- data_process.py
+++ data_process.py
@@ -1,4 +1,39 @@
 import pandas as pd
+import re, nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk import ngrams
 
+def normalizer(tweet):
+    only_letters = re.sub("[^a-zA-Z]", " ",tweet) 
+    tokens = nltk.word_tokenize(only_letters)[2:]
+    lower_case = [l.lower() for l in tokens]
+    
+    stop_words = set(stopwords.words('english'))
+    wordnet_lemmatizer = WordNetLemmatizer()
+    
+    filtered_result = list(filter(lambda l: l not in stop_words, lower_case))
+    lemmas = [wordnet_lemmatizer.lemmatize(t) for t in filtered_result]
+    return lemmas
+
+  
+def ngrams(input_list):
+    #onegrams = input_list
+    bigrams = [' '.join(t) for t in list(zip(input_list, input_list[1:]))]
+    trigrams = [' '.join(t) for t in list(zip(input_list, input_list[1:], input_list[2:]))]
+    return bigrams+trigrams
+ 
 tweet = pd.read_csv("Tweet.csv")
 print(list("tweet.column.values"))
+
+sentiment_counts = tweets.airline_sentiment.value_counts()
+number_of_tweets = tweets.tweet_id.count()
+print(sentiment_counts)
+
+
+pd.set_option('display.max_colwidth', -1) # Setting this so we can see the full content of cells
+tweets['normalized_tweet'] = tweets.text.apply(normalizer)
+tweets[['text','normalized_tweet']].head()
+
+tweets['grams'] = tweets.normalized_tweet.apply(ngrams)
+print(tweets[['grams']].head())
